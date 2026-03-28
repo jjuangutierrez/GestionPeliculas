70b7c7326788195be122eda977ff7adea47c5489
jjuangutierrez <juanmiguelgutiererz47@gmail.com>
2026-03-28 10:01:19 -0500

    api url actualizada

--- Gestionpeliculas.web/src/service/api.ts
+++ Gestionpeliculas.web/src/service/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  //baseURL: "http://localhost:4000/api",
+  baseURL: "https://gestionpeliculas-8n0v.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
